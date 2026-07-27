94e0fb50b9c6565fe8439c74b287ca86b9289fb2
abdirashiid <abdirashiidbile5@gmail.com>
2026-07-27 20:46:54 +0300

    feat: initial commit — Baladweyne AMS v1.0 full-stack agricultural management system

--- server/dist/routes/reportRoutes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const reportController_1 = require("../controllers/reportController");
-const auth_1 = require("../middleware/auth");
-const router = (0, express_1.Router)();
-router.use(auth_1.authenticateToken);
-router.get('/generate', reportController_1.generateReport);
-exports.default = router;
